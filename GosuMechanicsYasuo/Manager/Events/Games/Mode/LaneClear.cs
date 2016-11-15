81f3404a1e73da2ee42206468015e50671180ce1
NightMoon032 <407368027@qq.com>
2016-11-16 06:31:40 +0800

    Yasuo LaneClear Update?

--- GosuMechanicsYasuo/Manager/Events/Games/Mode/LaneClear.cs
+++ GosuMechanicsYasuo/Manager/Events/Games/Mode/LaneClear.cs
@@ -3,6 +3,7 @@
     using System.Linq;
     using Common;
     using Spells;
+    using LeagueSharp;
     using LeagueSharp.Common;
     using static Common.Common;
 
@@ -14,25 +15,11 @@
 
             if (minions.Any())
             {
-                if (Menu.Item("LaneClearQ", true).GetValue<bool>() && Q.IsReady() && !SpellManager.HaveQ3)
+                if (Menu.Item("LaneClearItems", true).GetValue<bool>())
                 {
-                    var qFarm = MinionManager.GetBestLineFarmLocation(minions.Select(x => x.Position.To2D()).ToList(),
-                        Q.Width, Q.Range);
-
-                    if (qFarm.MinionsHit >= 1)
+                    foreach (var min in minions.Where(x => x.DistanceToPlayer() <= 400))
                     {
-                        Q.Cast(qFarm.Position, true);
-                    }
-                }
-
-                if (Menu.Item("LaneClearQ3", true).GetValue<bool>() && Q3.IsReady() && SpellManager.HaveQ3)
-                {
-                    var q3Farm = MinionManager.GetBestLineFarmLocation(minions.Select(x => x.Position.To2D()).ToList(),
-                        Q3.Width, Q3.Range);
-
-                    if (q3Farm.MinionsHit >= Menu.Item("LaneClearQ3count", true).GetValue<Slider>().Value)
-                    {
-                        Q3.Cast(q3Farm.Position, true);
+                        UseItems(min);
                     }
                 }
 
@@ -43,7 +30,7 @@
                         if (Menu.Item("LaneClearETurret", true).GetValue<bool>() || !UnderTower(PosAfterE(min)))
                         {
                             var predHealth = HealthPrediction.LaneClearHealthPrediction(min,
-                                (int) (Me.Distance(min.Position)*1000/2000));
+                                (int)(Me.Distance(min.Position) * 1000 / 2000));
 
                             if (predHealth <= SpellManager.GetEDmg(min) && !isDangerous(min, 600))
                             {
@@ -53,11 +40,51 @@
                     }
                 }
 
-                if (Menu.Item("LaneClearItems", true).GetValue<bool>())
+                if (Menu.Item("LaneClearQ", true).GetValue<bool>() && Q.IsReady() && !SpellManager.HaveQ3)
                 {
-                    foreach (var min in minions.Where(x => x.DistanceToPlayer() <= 400))
+                    if (!IsDashing)
                     {
-                        UseItems(min);
+                        var qFarm =
+                            MinionManager.GetBestLineFarmLocation(minions.Select(x => x.Position.To2D()).ToList(),
+                                Q.Width, Q.Range);
+
+                        if (qFarm.MinionsHit >= 1)
+                        {
+                            Q.Cast(qFarm.Position, true);
+                        }
+                    }
+                    else
+                    {
+                        var qminions = MinionManager.GetMinions(lastEPos, 220);
+
+                        if (qminions.Count >= 2)
+                        {
+                            Utility.DelayAction.Add(50 + Game.Ping, () => Q.Cast(Me.Position));
+                        }
+                    }
+                }
+
+                if (Menu.Item("LaneClearQ3", true).GetValue<bool>() && Q3.IsReady() && SpellManager.HaveQ3)
+                {
+                    if (!IsDashing)
+                    {
+                        var q3Farm =
+                            MinionManager.GetBestLineFarmLocation(minions.Select(x => x.Position.To2D()).ToList(),
+                                Q3.Width, Q3.Range);
+
+                        if (q3Farm.MinionsHit >= Menu.Item("LaneClearQ3count", true).GetValue<Slider>().Value)
+                        {
+                            Q3.Cast(q3Farm.Position, true);
+                        }
+                    }
+                    else
+                    {
+                        var q3minions = MinionManager.GetMinions(lastEPos, 220);
+
+                        if (q3minions.Count >= 2)
+                        {
+                            Utility.DelayAction.Add(50 + Game.Ping, () => Q3.Cast(Me.Position));
+                        }
                     }
                 }
             }
