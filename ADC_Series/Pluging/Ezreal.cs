33eb7127c8abfb745e0685fb39b9936ac1bfc478
NightMoon032 <407368027@qq.com>
2016-11-20 21:10:38 +0800

    Ezreal Auto Stack Range

--- ADC_Series/Pluging/Ezreal.cs
+++ ADC_Series/Pluging/Ezreal.cs
@@ -191,8 +191,8 @@
                     return;
                 }
 
-                if (Me.CountEnemiesInRange(1000) == 0 &&
-                    !MinionManager.GetMinions(Me.Position, Q.Range, MinionTypes.All, MinionTeam.NotAlly).Any())
+                if (Me.CountEnemiesInRange(Q.Range + E.Range) == 0 &&
+                    !MinionManager.GetMinions(Me.Position, Q.Range + 200, MinionTypes.All, MinionTeam.NotAlly).Any())
                 {
                     if (Menu.Item("AutoStackQ", true).GetValue<bool>() && Q.IsReady() &&
                         Utils.TickCount - lastSpellCast > 4100)
