26a3f8a8f9d6bfb8a9e5e9d69fc156bdc5f5a406
NightMoon032 <407368027@qq.com>
2016-11-26 20:52:17 +0800

    Update Twitch

--- ADC_Series/Pluging/Twitch.cs
+++ ADC_Series/Pluging/Twitch.cs
@@ -168,15 +168,16 @@
 
             if (CheckTarget(target, R.Range))
             {
-                if (Menu.Item("ComboQ", true).GetValue<bool>() && Q.IsReady())
+                if (Menu.Item("ComboQ", true).GetValue<bool>() && Q.IsReady() &&
+                    target.DistanceToPlayer() <= Menu.Item("ComboQRange", true).GetValue<Slider>().Value &&
+                    (Me.CountEnemiesInRange(Orbwalking.GetRealAutoAttackRange(Me) + 150) >= 2 || Me.HealthPercent <= 50 ||
+                     (target.HealthPercent <= 80 && target.HealthPercent >= 30)))
                 {
-                    if (target.DistanceToPlayer() <= Menu.Item("ComboQRange", true).GetValue<Slider>().Value)
-                    {
-                        Q.Cast();
-                    }
+                    Q.Cast();
                 }
 
-                if (Menu.Item("ComboW", true).GetValue<bool>() && W.IsReady() && target.IsValidTarget(W.Range))
+                if (Menu.Item("ComboW", true).GetValue<bool>() && W.IsReady() && target.IsValidTarget(W.Range) &&
+                    Me.Mana >= W.Instance.ManaCost + E.Instance.ManaCost + R.Instance.ManaCost)
                 {
                     W.CastTo(target);
                 }
@@ -273,9 +274,13 @@
                     MinionOrderTypes.MaxHealth);
 
                 foreach (
-                    var mob in mobs.Where(x => !x.Name.ToLower().Contains("mini") && x.DistanceToPlayer() <= E.Range))
+                    var mob in
+                    mobs.Where(
+                        x =>
+                            !x.Name.ToLower().Contains("mini") && x.DistanceToPlayer() <= E.Range &&
+                            x.HasBuff("TwitchDeadlyVenom")))
                 {
-                    if (E.IsKillable(mob))
+                    if (mob.Health < E.GetDamage(mob))
                     {
                         E.Cast();
                     }
