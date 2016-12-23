e2dbbbf6251a98d49e37064d5f84dde47bd75d33
NightMoon032 <407368027@qq.com>
2016-12-23 15:00:47 +0800

    Fix Ahri R

--- xSaliceResurrected_Rework/Pluging/Ahri.cs
+++ xSaliceResurrected_Rework/Pluging/Ahri.cs
@@ -284,14 +284,14 @@
                         return;
                     }
 
-                    var dashVector = Player.Position +
-                                         Vector3.Normalize(target.ServerPosition - Player.Position) * 425;
-                    if (Player.Distance(target.ServerPosition) <= R.Range &&
-                        Player.GetSpellDamage(target, SpellSlot.R) > target.Health && R.IsReady() && _rOn &&
-                        target.Distance(dashVector) < 425 && R.IsReady())
-                    {
-                        R.Cast(dashVector);
-                    }
+                    // var dashVector = Player.Position +
+                                         // Vector3.Normalize(target.ServerPosition - Player.Position) * 425;
+                    // if (Player.Distance(target.ServerPosition) <= R.Range &&
+                        // Player.GetSpellDamage(target, SpellSlot.R) > target.Health && R.IsReady() && _rOn &&
+                        // target.Distance(dashVector) < 425 && R.IsReady())
+                    // {
+                        // R.Cast(dashVector);
+                    // }
                 }
             }
         }
